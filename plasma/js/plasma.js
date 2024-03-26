52f9e6c4593c936ecd683379ba85fe7b8226c42f
Stefan Pettersson <stefpet@gmail.com>
2024-03-26 16:36:28 +0100

    Static palette for plasma

--- plasma/js/plasma.js
+++ plasma/js/plasma.js
@@ -5,13 +5,10 @@
   canvas.height = window.innerHeight; // canvas.clientHeight;
   var ctx = canvas.getContext('2d');
 
+  let palette = getGradient("gradient");
+  let paletteShift = 0;
+
   var size = 10;
-
-  let color = [255, 32, 255];
-
-  for (let i = 0; i < 100; i++) {
-    draw(i, 5, color.map(c => Math.round(i * c % 255)));
-  }
 
   function draw(x, y, rgb) {
     ctx.fillStyle = 'rgb(' + rgb + ')';
@@ -28,38 +25,45 @@
         + 128 + (128 * Math.sin(y / 8.0))
       ) / 2;
 
-      let color = [
-        0,
-        v,
-        0
-      ];
+      let color = palette[(Math.round(v) + paletteShift) % palette.length];
 
       draw(x, y, color);
     }
   }
 
-})('plasma');
+  function getGradient(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    var ctx = canvas.getContext('2d');
 
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
 
-/*
+    gradient.addColorStop(0, "black");
+    gradient.addColorStop(0.5, "red");
+    gradient.addColorStop(0.7, "yellow");
+    gradient.addColorStop(1.0, "black");
 
-int main(int argc, char *argv[])
-{
-  screen(256, 256, 0, "Plasma");
+    // Set the fill style and draw a rectangle
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  for(int y = 0; y < h; y++)
-  for(int x = 0; x < w; x++)
-  {
-    int color = int
-    (
-        128.0 + (128.0 * sin(x / 8.0))
-      + 128.0 + (128.0 * sin(y / 8.0))
-    ) / 2;
-    pset(x, y, ColorRGB(color, color, color));
+    let array = [];
+
+    for (let i = 0; i < canvas.width; i++) {
+      const color = ctx.getImageData(i, 0, 1, 1).data;
+      array.push(
+        [
+          color[0],
+          color[1],
+          color[2]
+        ]
+      )
+    }
+
+    canvas.remove();
+
+    return array;
   }
-  redraw();
-  sleep();
-  return(0);
-}
 
-*/
+})('plasma');
